8360246b1b1e2560d085d96c819d95e1a18d1f83
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2024-06-23 11:03:47 +0000

    Constructor Injection, Component Scanning & Setter Injection

---
+++ 02-spring-boot-spring-core/01-Constructor-Injection/src/main/java/com/example/springcoredemo/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkoutString() {
+        return myCoach.getDailyWorkout();
+    }
+}
